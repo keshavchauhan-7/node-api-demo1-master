0ca83bdb52886106c26c6d88a13e5f3a295d2a9c
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2025-09-17 16:13:31 +0530

    a

--- server.js
+++ server.js
@@ -5,24 +5,24 @@ const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client
 
 dotenv.config(); // load .env if available
 
-async function getDbUri() {
-  if (process.env.MONGO_URI) {
-    // local dev
-    //console.log("Using local .env MONGO_URI" + process.env.MONGO_URI );
-    return process.env.MONGO_URI;
-  } else {
-    // production (App Runner)
-      const client = new SecretsManagerClient({ region: "ap-south-1" });
-    const command = new GetSecretValueCommand({ SecretId: "mongoDbSecret" });
-    const response = await client.send(command);
-    return JSON.parse(response.SecretString).MONGO_URI;
-  }
-}
+// async function getDbUri() {
+//   if (process.env.MONGO_URI) {
+//     // local dev
+//     //console.log("Using local .env MONGO_URI" + process.env.MONGO_URI );
+//     return process.env.MONGO_URI;
+//   } else {
+//     // production (App Runner)
+//       const client = new SecretsManagerClient({ region: "ap-south-1" });
+//     const command = new GetSecretValueCommand({ SecretId: "mongoDbSecret" });
+//     const response = await client.send(command);
+//     return JSON.parse(response.SecretString).MONGO_URI;
+//   }
+// }
 
 (async () => {  
   try {
-    const mongoUri = await getDbUri();
-    await mongoose.connect(mongoUri);
+    // const mongoUri = await getDbUri();
+    // await mongoose.connect(mongoUri);
     console.log("✅ MongoDB connected");
 
     const PORT = process.env.PORT || 3000;
