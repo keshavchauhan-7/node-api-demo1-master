753584ecd8405ea5d7179534c4e4998cd2631d8a
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2025-09-18 15:06:12 +0530

    hello

--- src/app.js
+++ src/app.js
@@ -1,3 +1,32 @@
+// const express = require("express");
+// const cors = require("cors");
+// const mongoose = require("mongoose");
+// const userRoutes = require("./routes/userRoutes");
+
+// const app = express();
+
+// // Middleware
+
+// // Allow all origins (for testing)
+// app.use(cors());
+
+// // Or restrict to your frontend domain
+// // app.use(cors({ origin: "https://your-frontend.com" }));
+
+// app.use(express.json());
+
+// // Health endpoint
+// app.get("/health", (req, res) => {
+//   res.json({ status: "API is healthy 🚀! Hurreyyy!!!!! Beanstalk is working working" });
+// });
+
+// // Routes
+// app.use("/api/users", userRoutes);
+
+// module.exports = app;
+
+
+
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -17,7 +46,7 @@ app.use(express.json());
 
 // Health endpoint
 app.get("/health", (req, res) => {
-  res.json({ status: "API is healthy 🚀! Hurreyyy!!!!! Beanstalk is working working" });
+  res.json({ status: "API is healthy 🚀! - hurrrryyyyyyyy !" });
 });
 
 // Routes
